240eb065cef6c7de73c1f71f4d564d338c170903
BangdeOnly <154625091+BangdeOnly@users.noreply.github.com>
2025-08-11 15:54:39 +0800

    Update main.py

--- main.py
+++ main.py
@@ -90,5 +90,6 @@ def list_keys():
     return jsonify({"status": "success", "data": db})
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
+    import os
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
